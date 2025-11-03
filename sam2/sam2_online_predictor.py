969dad578a42d4c75624f87821497d37aabfbe99
Leon Kim <leonmkim@seas.upenn.edu>
2025-11-03 16:40:01 -0500

    Set default values to True for memory and prompt encoder compilation in SAM2OnlinePredictorVOS

--- sam2/sam2_online_predictor.py
+++ sam2/sam2_online_predictor.py
@@ -1253,10 +1253,10 @@ class SAM2OnlinePredictorVOS(SAM2OnlinePredictor):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.compile_memory_encoder = kwargs.get("compile_memory_encoder", False)
-        self.compile_memory_attention = kwargs.get("compile_memory_attention", False)
-        self.compile_prompt_encoder = kwargs.get("compile_prompt_encoder", False)
-        self.compile_mask_decoder = kwargs.get("compile_mask_decoder", False)
+        self.compile_memory_encoder = kwargs.get("compile_memory_encoder", True)
+        self.compile_memory_attention = kwargs.get("compile_memory_attention", True)
+        self.compile_prompt_encoder = kwargs.get("compile_prompt_encoder", True)
+        self.compile_mask_decoder = kwargs.get("compile_mask_decoder", True)
         self._compile_all_components()
 
     def _compile_all_components(self):
